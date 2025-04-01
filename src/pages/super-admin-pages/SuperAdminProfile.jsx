26c988cbfbf1170298f7f154be320c110707daea
AhmedTharwat <a.sarwat100@gmail.com>
2025-04-01 23:59:48 +0200

    finished super-admin profile

--- src/pages/super-admin-pages/SuperAdminProfile.jsx
+++ src/pages/super-admin-pages/SuperAdminProfile.jsx
@@ -1,5 +1,18 @@
+import { SUPER_ADMIN_PAGES_PROPERTIES } from "../../utils/constants";
+import ContentHeader from "../../ui/ContentHeader";
+import SuperAdminProfileContent from "../../features/super-admin-features/super-profile/SuperAdminProfileContent";
+
 function SuperAdminProfile() {
-  return <div></div>;
+  return (
+    <>
+      <ContentHeader
+        title={SUPER_ADMIN_PAGES_PROPERTIES.PROFILE.title}
+        description={SUPER_ADMIN_PAGES_PROPERTIES.PROFILE.description}
+        button={false}
+      />
+      <SuperAdminProfileContent />
+    </>
+  );
 }
 
 export default SuperAdminProfile;
