7a7a09c1cfc018bd08101736c0a8fbd9a5fd824c
AhmedTharwat <a.sarwat100@gmail.com>
2025-04-27 03:44:05 +0300

    fetching news for superadmin setup

---
+++ src/features/super-admin-features/super-dashboard/SuperAdminDashboardContent.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Div = styled.div``;
+
+function SuperAdminDashboardContent() {
+  return <div></div>;
+}
+
+export default SuperAdminDashboardContent;
